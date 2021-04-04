dcbd16faedb199b003d27ab1e49535808519612a
symoon94 <msy0128@gmail.com>
2021-04-03 23:25:22 -0700

    add visit

--- handlers/person.py
+++ handlers/person.py
@@ -1,11 +1,13 @@
 
 from flask import request
+from flask import jsonify
+from sqlalchemy import func
 
 from app import app
-from models.models import Person, Death
-from sqlalchemy import func
+
+from models.models import Person, Death, Visit
+
 import json
-from flask import jsonify
 
 
 @app.route('/person')
@@ -13,12 +15,15 @@ def get_person():
     if request.method == 'GET':
         query = app.db.query(Person)
 
-        gender = query.with_entities(Person.gender_concept_id, func.count(
-            Person.gender_concept_id)).group_by(Person.gender_concept_id).all()
-        race = query.with_entities(Person.race_concept_id, func.count(
-            Person.race_concept_id)).group_by(Person.race_concept_id).all()
-        ethnicity = query.with_entities(Person.ethnicity_concept_id, func.count(
-            Person.ethnicity_concept_id)).group_by(Person.ethnicity_concept_id).all()
+        gender = Person.gender_concept_id
+        race = Person.race_concept_id
+        ethnicity = Person.ethnicity_concept_id
+
+        gender = query.with_entities(
+            gender, func.count(gender)).group_by(gender).all()
+        race = query.with_entities(race, func.count(race)).group_by(race).all()
+        ethnicity = query.with_entities(
+            ethnicity, func.count(ethnicity)).group_by(ethnicity).all()
 
         gender = json.loads(json.dumps(dict(gender)))
         race = json.loads(json.dumps(dict(race)))
@@ -29,7 +34,22 @@ def get_person():
         return jsonify({"total": query.count(), "gender": gender, "race": race, "ethnicity": ethnicity, "death": death})
 
 
-@app.route('/api')
+@ app.route('/visit')
+def get_visit():
+    if request.method == 'GET':
+        query = app.db.query(Visit)
+
+        visit = Visit.visit_concept_id
+
+        visit = query.with_entities(visit, func.count(
+            visit)).group_by(visit).all()
+
+        visit = json.loads(json.dumps(dict(visit)))
+
+        return jsonify(visit)
+
+
+@ app.route('/api')
 def get_api():
     if request.method == 'GET':
         return jsonify({"count": 'tmp'})
--- models/models.py
+++ models/models.py
@@ -15,3 +15,10 @@ class Death(Base):
     __tablename__ = 'death'
 
     person_id = Column(BigInteger(), primary_key=True)
+
+
+class Visit(Base):
+    __tablename__ = 'visit_occurrence'
+
+    visit_occurrence_id = Column(BigInteger(), primary_key=True)
+    visit_concept_id = Column(Integer())
