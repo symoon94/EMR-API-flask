3697c7c6a851ee40545e8dd398ea9e9f763dcd61
symoon94 <msy0128@gmail.com>
2021-04-03 23:14:20 -0700

    add death

--- handlers/person.py
+++ handlers/person.py
@@ -2,7 +2,7 @@
 from flask import request
 
 from app import app
-from models.person import Person
+from models.models import Person, Death
 from sqlalchemy import func
 import json
 from flask import jsonify
@@ -13,17 +13,23 @@ def get_person():
     if request.method == 'GET':
         query = app.db.query(Person)
 
-        gender = query.with_entities(Person.gender_concept_id, func.count(Person.gender_concept_id)).group_by(Person.gender_concept_id).all()
-        race =  query.with_entities(Person.race_concept_id, func.count(Person.race_concept_id)).group_by(Person.race_concept_id).all()
-        ethnicity = query.with_entities(Person.ethnicity_concept_id, func.count(Person.ethnicity_concept_id)).group_by(Person.ethnicity_concept_id).all()
-        
+        gender = query.with_entities(Person.gender_concept_id, func.count(
+            Person.gender_concept_id)).group_by(Person.gender_concept_id).all()
+        race = query.with_entities(Person.race_concept_id, func.count(
+            Person.race_concept_id)).group_by(Person.race_concept_id).all()
+        ethnicity = query.with_entities(Person.ethnicity_concept_id, func.count(
+            Person.ethnicity_concept_id)).group_by(Person.ethnicity_concept_id).all()
+
         gender = json.loads(json.dumps(dict(gender)))
         race = json.loads(json.dumps(dict(race)))
         ethnicity = json.loads(json.dumps(dict(ethnicity)))
 
-        return jsonify({"total": query.count(), "gender":gender, "race":race, "ethnicity":ethnicity})
+        death = app.db.query(Death).count()
+
+        return jsonify({"total": query.count(), "gender": gender, "race": race, "ethnicity": ethnicity, "death": death})
+
 
 @app.route('/api')
 def get_api():
     if request.method == 'GET':
-        return jsonify({"count":'tmp'})
+        return jsonify({"count": 'tmp'})
