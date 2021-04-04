c0c2542458614c0ee63518447d3bc1acf3d61b38
symoon94 <msy0128@gmail.com>
2021-04-03 23:07:08 -0700

    fix response

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-import sys
 from flask import request
+import sys
+
 
 sys.path.append("..")
 
 app = Flask(__name__)
-
 from handlers import person
-
--- handlers/person.py
+++ handlers/person.py
@@ -5,6 +5,8 @@ from app import app
 from models.person import Person
 from sqlalchemy import func
 import json
+from flask import jsonify
+
 
 @app.route('/person')
 def get_person():
@@ -14,14 +16,14 @@ def get_person():
         gender = query.with_entities(Person.gender_concept_id, func.count(Person.gender_concept_id)).group_by(Person.gender_concept_id).all()
         race =  query.with_entities(Person.race_concept_id, func.count(Person.race_concept_id)).group_by(Person.race_concept_id).all()
         ethnicity = query.with_entities(Person.ethnicity_concept_id, func.count(Person.ethnicity_concept_id)).group_by(Person.ethnicity_concept_id).all()
-
+        
         gender = json.loads(json.dumps(dict(gender)))
         race = json.loads(json.dumps(dict(race)))
         ethnicity = json.loads(json.dumps(dict(ethnicity)))
 
-        return {"total": query.count(), "gender":gender, "race":race, "ethnicity":ethnicity}
+        return jsonify({"total": query.count(), "gender":gender, "race":race, "ethnicity":ethnicity})
 
 @app.route('/api')
 def get_api():
     if request.method == 'GET':
-        return {"count":'tmp'}
+        return jsonify({"count":'tmp'})
